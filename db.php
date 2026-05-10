37c9f44e77727c90804e8c7fb19fe94940e65c6e
ab124-rgb <ab124@illinois.edu>
2026-05-10 18:14:49 -0500

    Update db.php

--- db.php
+++ db.php
@@ -1,7 +1,7 @@
 <?php
 $host = "localhost";
 $username = "mapping207_group19_user";
-$password = "xamraf-wiZjem-5tecqy";
+$password = "USEYOURPASSWORD";
 $database = "mapping207_group19_events";
 
 $conn = new mysqli($host, $username, $password, $database);
